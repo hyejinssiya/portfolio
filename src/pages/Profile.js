f2ca55ff8dba5646caef0f550c53690c1ce1a79a
hyejinssiya <khl0211@gmail.com>
2022-10-27 09:45:35 +0900

    -

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,44 +4,45 @@ const Profile = (props) => {
   return (
     <>
         <h2 className="profile_title">웹퍼블리셔<br /> 권혜진</h2>
-        <ul className="profile_list">
-          <li>
-            <dl>
-              <dt>Skill</dt>
-              <dd>HTML</dd>
-              <dd>CSS</dd>
-              <dd>SCSS</dd>
-              <dd>jQuery</dd>
-              <dd>Javascript</dd>
-              <dd>React</dd>
-              <dd>Git</dd>
-              <dd>nexacro</dd>
-              <dd>웹접근성(Web Accessibility)</dd>
-              <dd>크로스브라우징</dd>
-            </dl>
-          </li>
-          <li>
-            <dl>
-              <dt>Tool</dt>
-              <dd>vscode</dd>
-              <dd>Github</dd>
-              <dd>gitlab</dd>
-              <dd>bitbucket</dd>
-              <dd>Photoshop</dd>
-              <dd>XD</dd>
-              <dd>Figma</dd>
-              <dd>Zeplin</dd>
-            </dl>
-          </li>
-          <li className="career">
-            <dl>
-              <dt>Career</dt>
-              <dd>2019.07 ~ 2022.06 오픈필드</dd>
-              <dd>담당업무 : 웹사이트 구축 및 유지보수</dd>
-              <dd></dd>
-            </dl>
-          </li>
-        </ul>  
+        <div className="profile_list">
+          <div>
+            <p className="tit">Skill</p>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>SCSS</li>
+              <li>jQuery</li>
+              <li>Javascript</li>
+              <li>React</li>
+              <li>Git</li>
+              <li>nexacro</li>
+              <li>웹접근성(Web Accessibility)</li>
+              <li>크로스브라우징</li>
+            </ul>
+          </div>
+          <div>
+            <p className="tit">Tool</p>
+            <ul>
+              <li>vscode</li>
+                <li>Github</li>
+                <li>gitlab</li>
+                <li>bitbucket</li>
+                <li>Photoshop</li>
+                <li>XD</li>
+                <li>Figma</li>
+                <li>Zeplin</li>
+            </ul>  
+          </div>
+          <div className="career">
+            <p className="tit">Career</p>
+            <ul>
+              <li>2022.07 ~ 서밋코퍼레이션</li>
+              <li>담당업무 : 웹사이트 구축 및 유지보수</li>
+              <li>2019.07 ~ 2022.06 오픈필드</li>
+              <li>담당업무 : 웹사이트 구축 및 유지보수</li>
+            </ul>
+          </div>
+        </div>  
     </>
   )
 }
